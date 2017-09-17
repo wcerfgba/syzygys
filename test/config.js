114ad384d835aaf1020074f8efee3fe231055799
John L. Preston <wcerfgba@riseup.net>
2017-09-17 15:33:30 +0100

    Lint and ES6ify.

--- test/config.js
+++ test/config.js
@@ -1,12 +1,10 @@
-import { fs, tmp } from './support/util'
-
+import { describe, it, fs, tmp } from './support/util';
 import { Config } from '../lib/';
 
 describe('Config', function () {
-
   const validAttrs = {
     feeds: [],
-    templatePaths: []
+    templatePaths: [],
   };
 
   it('has feeds', function () {
@@ -22,19 +20,16 @@ describe('Config', function () {
   });
 
   describe('.fromFile', function () {
-
     it('creates a Config from a file containing JSON', function () {
       const validConfig = new Config(validAttrs);
       return tmp.fileAsync()
-                .bind({})
-                .then(function (file) {
-                  this.file = file;
-                  return fs.writeFileAsync(file, JSON.stringify(validConfig));
-                })
-                .then(function () { return Config.fromFile(this.file); })
-                .then((config) => config.should.deepEqual(validConfig));
+        .bind({})
+        .then(function (file) {
+          this.file = file;
+          return fs.writeFileAsync(file, JSON.stringify(validConfig));
+        })
+        .then(function () { return Config.fromFile(this.file); })
+        .then(config => config.should.deepEqual(validConfig));
     });
-
-  })
-
+  });
 });
