5c2ce98a55a373e6bd88f99d5617b8076d8e5d26
John L. Preston <wcerfgba@riseup.net>
2017-09-17 15:35:36 +0100

    Finish specing feed.

--- test/feed.js
+++ test/feed.js
@@ -1,31 +1,136 @@
-import './support/util'
-
-import { Feed } from '../lib/';
+import fetch from 'node-fetch';
+import {
+  describe,
+  it,
+  context,
+  before,
+  after,
+  fs,
+  stub,
+} from './support/util';
+import { Feed, FeedItem } from '../lib';
 
 describe('Feed', function () {
+  const validAttrs = {
+    name: 'Test Feed',
+    url: 'http://example.com/feed',
+    description: 'A test feed',
+    tags: ['test'],
+  };
+
+  const invalidUrlAttrs = {
+    name: 'Test Feed with invalid URL',
+    url: 'sgssdfsdxample.csdfsdfom/notafeed',
+  };
+
+  const missingUrlAttrs = {
+    name: 'Just name',
+  };
 
   it('has a name', function () {
-    feed = new Feed();
+    const feed = new Feed(validAttrs);
 
     feed.should.have.property('name').which.is.a.String();
   });
 
   it('has a url', function () {
-    feed = new Feed();
+    const feed = new Feed(validAttrs);
 
     feed.should.have.property('url').which.is.a.String();
   });
 
   it('has a description', function () {
-    feed = new Feed();
+    const feed = new Feed(validAttrs);
 
     feed.should.have.property('description').which.is.a.String();
   });
 
   it('has tags', function () {
-    feed = new Feed();
+    const feed = new Feed(validAttrs);
 
     feed.should.have.property('tags').which.is.an.Array();
   });
 
+  it('can be retrieved', function () {
+    const feed = new Feed(validAttrs);
+
+    feed.should.have.property('retrieve').which.is.a.Function();
+  });
+
+  describe('#retrieve', function () {
+    it('returns a Promise', function () {
+      const feed = new Feed(validAttrs);
+      const feedItems = feed.retrieve();
+
+      return feedItems.should.be.a.Promise();
+    });
+
+    context('invalid url', function () {
+      it('rejects with an error', function () {
+        const feed = new Feed(invalidUrlAttrs);
+        const feedItems = feed.retrieve();
+
+        return feedItems.should.be.rejectedWith(Error);
+      });
+    });
+
+    context('missing url', function () {
+      it('rejects with an error', function () {
+        const feed = new Feed(missingUrlAttrs);
+        const feedItems = feed.retrieve();
+
+        return feedItems.should.be.rejectedWith(Error);
+      });
+    });
+
+    context('network problem', function () {
+      const originalFetchPromise = fetch.Promise;
+
+      before(function () {
+        stub(fetch, 'Promise').rejects(new Error('Network error'));
+      });
+
+      after(function () {
+        fetch.Promise = originalFetchPromise;
+      });
+
+      it('rejects with an error', function () {
+        const feed = new Feed(validAttrs);
+        const feedItems = feed.retrieve();
+
+        return feedItems.should.be.rejectedWith(new Error('Network error'));
+      });
+    });
+
+    context('valid url', function () {
+      const originalFetchPromise = fetch.Promise;
+
+      before(function () {
+        const fixture =
+          fs.readFileAsync(__dirname.join('/fixture/feed.rss'))
+            .then(fixtureData => ({
+              ok: true,
+              status: 200,
+              statusText: 'OK',
+              body: fixtureData,
+            }));
+        stub(fetch, 'Promise').resolves(fixture);
+      });
+
+      after(function () {
+        fetch.Promise = originalFetchPromise;
+      });
+
+      it('fulfilled with an array of feedItems', function () {
+        const feed = new Feed(validAttrs);
+        const feedItems = feed.retrieve();
+
+        return feedItems.should.be.fulfilled()
+          .then((result) => {
+            result.should.be.Array();
+            result[0].should.be.an.instanceof(FeedItem);
+          });
+      });
+    });
+  });
 });
