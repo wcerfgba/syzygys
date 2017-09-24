07203c7d6c46dd7339df6aa705ecdc0adc6605df
John L. Preston <wcerfgba@riseup.net>
2017-09-24 15:07:44 +0100

    No need to export path just in test support

--- test/support/util.js
+++ test/support/util.js
@@ -4,5 +4,4 @@ import { Promise } from '../../lib/util';
 export * from '../../lib/util';
 export * from 'should';
 export * from 'sinon';
-export { default as path } from 'path';
 export const tmp = Promise.promisifyAll(originalTmp);
